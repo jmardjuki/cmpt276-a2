ed121cf4dd6c3879be69d360d6c1e369592f439a
Janet Mardjuki <mardjuki@sfu.ca>
2016-06-02 01:47:53 -0700

    Added Students by following  widgets

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20160602074456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "stock"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
